05568b4f874b1e68a131c145049bedaf3b1b167c
Chavi-Kothari <141266025+Chavi-Kothari@users.noreply.github.com>
2023-08-03 00:22:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
-
+import { fileURLToPath } from "url"
 conncetDB();
 const app=express();
-
+//esmodule file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'))
